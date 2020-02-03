1237814d48325a480922704c45b79631ee4456c2
Miguel Guilherme <mguilherme82@gmail.com>
2020-02-03 09:37:33 +0000

    remove useless comments

--- chuck_norris_quote.py
+++ chuck_norris_quote.py
@@ -8,20 +8,12 @@ import requests
 
 
 def log_config():
-    """
-    Logging configuration.
-    """
     filename = f'{splitext(basename(__file__))[0]}.log'
     path: str = dirname(__file__)
     logging.basicConfig(filename=join(path, filename), level=logging.ERROR)
 
 
 def request_quote() -> str:
-    """
-    Retrieves a random Chuck Norris quote.
-
-    :return: a Chuck Norris quote
-    """
     response = requests.get(url='https://api.chucknorris.io/jokes/random')
     content: dict = response.json()
     return content['value']
